9248d1d40c0966649c25a170aa9fb9efaa593534
Diogenes santos <diogenescontatoofficial@hotmail.com>
2026-05-03 19:46:46 -0300

    criado os teste de repository, e as implementações necessária na TarefaRepositoryImpl para os teste das funcionalidades.

--- src/main/java/com/github/diogenesssantos/facilittecnologia/repository/TarefaRepositoryImpl.java
+++ src/main/java/com/github/diogenesssantos/facilittecnologia/repository/TarefaRepositoryImpl.java
@@ -2,29 +2,61 @@ package com.github.diogenesssantos.facilittecnologia.repository;
 
 import com.github.diogenesssantos.facilittecnologia.model.Status;
 import com.github.diogenesssantos.facilittecnologia.model.Tarefa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 import java.util.List;
 import java.util.Optional;
 
-public class TarefaRepositoryImpl implements TarefaRepositoryCustom{
+public class TarefaRepositoryImpl implements TarefaRepositoryCustom {
 
+    private EntityManager entityManager;
 
+    public TarefaRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public Optional<Tarefa> buscarPorTitulo(String titulo) {
-        //todo implementar buscar por titulo
-        return null;
+        var builder = entityManager.getCriteriaBuilder();
+        var query = builder.createQuery(Tarefa.class);
+        var root = query.from(Tarefa.class);
+        query.select(root);
+
+        query.where(builder.equal(builder.lower(root.get("titulo")), titulo.toLowerCase()));
+
+        TypedQuery<Tarefa> typedQuery = entityManager.createQuery(query);
+        var tarefa = typedQuery.getResultList();
+
+        return !tarefa.isEmpty() ? Optional.of(tarefa.getFirst()) : Optional.empty();
     }
 
     @Override
     public Optional<Tarefa> buscarPorDescricao(String descricao) {
-        //todo implementar buscar por titulo
-        return null;
+        var builder = entityManager.getCriteriaBuilder();
+        var query = builder.createQuery(Tarefa.class);
+        var root = query.from(Tarefa.class);
+        query.select(root);
+
+        query.where(builder.equal(builder.lower(root.get("descricao")), descricao.toLowerCase()));
+
+        var typedQuery = entityManager.createQuery(query);
+        var tarefa = typedQuery.getResultList();
+
+        return !tarefa.isEmpty() ? Optional.of(tarefa.getFirst()) : Optional.empty();
     }
 
     @Override
     public List<Tarefa> buscarPorStatus(Status status) {
-        return List.of();
+        var builder = entityManager.getCriteriaBuilder();
+        var query = builder.createQuery(Tarefa.class);
+        var root = query.from(Tarefa.class);
+        query.select(root);
+
+        query.where(builder.equal(root.get("status"), status));
+
+        var typedQuery = entityManager.createQuery(query);
+        return typedQuery.getResultList();
     }
 
 
--- src/test/java/com/github/diogenesssantos/facilittecnologia/TarefaRepositoryTest.java
+++ src/test/java/com/github/diogenesssantos/facilittecnologia/TarefaRepositoryTest.java
@@ -5,7 +5,10 @@ import com.github.diogenesssantos.facilittecnologia.config.TestIntegrationConfig
 import com.github.diogenesssantos.facilittecnologia.model.Status;
 import com.github.diogenesssantos.facilittecnologia.model.Tarefa;
 import com.github.diogenesssantos.facilittecnologia.repository.TarefaRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -44,20 +47,16 @@ public class TarefaRepositoryTest {
 
 
     @Test
-    @DisplayName("Deve retornar uma list de Tarefa, quando chamar o método findAll, contendo 20 tarefas.")
+    @DisplayName("Deve retornar uma list de Tarefa, quando chamar o método findAll.")
     void buscarTodas() {
-        var quantidadeExpectativa = 20;
-
         var listaTarefaBD = tarefaRepository.findAll();
-
         assertFalse(listaTarefaBD.isEmpty());
-        assertEquals(20, listaTarefaBD.size());
     }
 
 
-
     @Test
-    @DisplayName("Deve retornar uma Tarefa, quando chamar o método save, e a tarefa tem que conter um ID de número 21.")
+    @DisplayName("Deve retornar uma Tarefa, quando chamar o método save, e ao retornar a" +
+            " Tarefa tem que conter um ID de número 21.")
     void salvarTarefa() {
         var tarefaExpectatica = tarefa;
         var idExpectativa = 21L;
@@ -67,6 +66,110 @@ public class TarefaRepositoryTest {
         assertNotNull(tarefaPersistidaBD);
         assertEquals(idExpectativa, tarefaPersistidaBD.getId());
         assertEquals(tarefaExpectatica.getTitulo(), tarefaPersistidaBD.getTitulo());
+    }
+
+    @Test
+    @DisplayName("Deve retornar um Optional com uma Tarefa, quando chamar o método findById com um id valido.")
+    void buscarPorIdValido() {
+        var idExpectativa = 3L;
+
+        var optionaltarefaBD = tarefaRepository.findById(idExpectativa);
+
+        assertTrue(optionaltarefaBD.isPresent());
+        assertEquals(idExpectativa, optionaltarefaBD.get().getId());
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar um Optional vázio, quando chamar o método findById com um id que não existe no " +
+            "banco de dados.")
+    void buscarPorIdQueNaoExiste() {
+        var idExpectativa = 3233L;
+
+        var optionaltarefaBD = tarefaRepository.findById(idExpectativa);
+
+        assertTrue(optionaltarefaBD.isEmpty());
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar um Optional com uma Tarefa, quando chamar o método buscarPorTitulo e o titulo" +
+            "correlacione a uma tarefa no banco de dados.")
+    void buscarPorTitulo() {
+        var tituloExpectativa = "Documentação API";
+
+        var optionalTarefaBD = tarefaRepository.buscarPorTitulo(tituloExpectativa);
+
+        assertTrue(optionalTarefaBD.isPresent());
+        assertNotNull(optionalTarefaBD.get().getId());
+        assertEquals(tituloExpectativa, optionalTarefaBD.get().getTitulo());
+
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar um Optional vázio, quando chamar o método buscarPorTitulo e o titulo" +
+            " não correlacionar a uma Tarefa no banco de dados.")
+    void buscarPorTituloQueNaoExiste() {
+        var tituloExpectativa = "Titulo não existe";
+
+        var optionalTarefaBD = tarefaRepository.buscarPorTitulo(tituloExpectativa);
+
+        assertTrue(optionalTarefaBD.isEmpty());
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar um Optional com uma Tarefa, quando chamar o método buscarPorTitulo e o titulo" +
+            "correlacione a uma tarefa no banco de dados.")
+    void buscarPorDescricao() {
+        var descricaoExpectativa = "Harden endpoints e headers";
+
+        var optionalTarefaBD = tarefaRepository.buscarPorDescricao(descricaoExpectativa);
+
+        assertTrue(optionalTarefaBD.isPresent());
+        assertNotNull(optionalTarefaBD.get().getId());
+        assertEquals(descricaoExpectativa, optionalTarefaBD.get().getDescricao());
+
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar um Optional vázio, quando chamar o método buscarPorTitulo e o titulo" +
+            " não correlacionar a uma Tarefa no banco de dados.")
+    void buscarPorDescricaoQueNaoExiste() {
+        var descricaoExpectativa = "Descrição não existe";
+
+        var optionalTarefaBD = tarefaRepository.buscarPorDescricao(descricaoExpectativa);
+
+        assertTrue(optionalTarefaBD.isEmpty());
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar uma list de Tarefa, quando chamar o método buscarPorStatus e o status" +
+            " correlacionem a uma ou mais Tarefa no banco de dados.")
+    void buscarPorStatus(){
+        var statusExpectatica = Status.CONCLUIDO;
+
+        var listTarefaPorStatusBD = tarefaRepository.buscarPorStatus(statusExpectatica);
+
+        assertNotNull(listTarefaPorStatusBD);
+        assertTrue(listTarefaPorStatusBD
+                .stream()
+                .allMatch(tarefaStream -> tarefaStream.getStatus().equals(statusExpectatica) ));
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar uma list de Tarefa vázia, quando chamar o método buscarPorStatus e o status" +
+            " for ATRASADO.")
+    void buscarPorStatusAtrasado_Retornar_Lista_Vazia(){
+        var statusExpectatica = Status.ATRASADO;
+
+        var listTarefaPorStatusBD = tarefaRepository.buscarPorStatus(statusExpectatica);
+
+        assertTrue(listTarefaPorStatusBD.isEmpty());
     }
 
 }
